dc3adb8dad923fea9da86a782de56811b7fa07ff
Javier Montano Rodriguez <jamontanor@vortex.corp>
2024-09-18 12:57:28 -0600

    cambios

---
+++ backend/Mega/Models/Entities/Tecnicos.cs
@@ -0,0 +1,10 @@
+﻿namespace Project.Models.Entities
+{
+    public class Tecnicos
+    {
+        public int userID {  get; set; }
+        public string nombre {  get; set; }
+        public required string apellidos { get; set; }
+
+    }
+}
