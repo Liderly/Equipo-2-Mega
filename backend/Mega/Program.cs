3239999b376ff0e35aff08bbbe39b8a731e203ad
ArmandoG <armandopinedag@outlook.com>
2024-09-27 11:28:53 -0700

    add swagger configuration

--- backend/Mega/Program.cs
+++ backend/Mega/Program.cs
@@ -1,11 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using Mega.Data;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "MEGA API",
+        Description = "Web API service for tecnicos weekly bonus calculation"
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"; //assambly name
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile); //from app location, take xml file
+    c.IncludeXmlComments(xmlPath);
+    //rely on Reflection for creating xml files matching with our webapi proj
+});
 
 builder.Services.AddControllers();
 
@@ -22,7 +38,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MEGA API"); //'v1' is for speficied 'version' at SwaggerDoc
+        c.RoutePrefix = String.Empty; // goto swagger with root uri (escape '/swagger')
+    });
 }
 
 app.UseHttpsRedirection();
